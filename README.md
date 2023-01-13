2687474f387ca950f82fb7da16ba98b54b990cc5
Yiming Liu <YimingDesigner@gmail.com>
2023-01-13 19:25:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ArrayExtension
+
+A description of this package.
---
+++ Tests/ArrayExtensionTests/ArrayExtensionTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+@testable import ArrayExtension
+
+final class ArrayExtensionTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+    }
+}
